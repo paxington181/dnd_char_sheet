a356badb02688a4e8cbed09faf92ecde18a0618b
paxington181 <tsandrus@rogers.com>
2026-09-06 19:57:29 -0400

    Base frame setup

--- main.py
+++ main.py
@@ -2,6 +2,38 @@ import customtkinter as CTk
 import CTkToolTip as CTkTT
 from PIL import Image
 
+class spells_frame(CTk.Frame):
+    def __init__(self):
+        super().__init__()
+
+class inventory_frame(CTk.Frame):
+    def __init__(self):
+        super().__init__()
+
+class money_frame(CTk.Frame):
+    def __init__(self):
+        super().__init__()
+
+class other_abilities(CTk.Frame):
+    def __init__(self):
+        super().__init__()
+
+class class_abilities(CTk.Frame):
+    def __init__(self):
+        super().__init__()
+
+class feats_frame(CTk.Frame):
+    def __init__(self):
+        super().__init__()
+
+class stat_frame(CTk.Frame):
+    def __init__(self):
+        super().__init__()        
+
+class class_frame(CTk.Frame):
+    def __init__(self):
+        super().__init__()
+
 class Main(CTk.CTk):
     def __init__(self):
         super().__init__()
