e42c54675d231eb5c81704d2cf8dd84b698f5194
paxington181 <tsandrus@rogers.com>
2026-09-09 11:16:54 -0400

    More plumbing.

--- main.py
+++ main.py
@@ -41,18 +41,26 @@ class Main(CTk.CTk):
         self.title("D&D 2024 Character Sheet")
         self.geometry("100x100")
 
-        self.char_class = ["Barbarian"]
+        self.char_class = ["barbarian"]
         self.char_subclass = None
-        self.species = "Human"
-        self.background = "Acolyte"
+        self.species = "human"
+        self.size = "medium"
+        self.background = "acolyte"
         self.level = 1
         self.experience = 0
         self.class_level = [1]
         self.max_hp = 13
+        self.current_hp = 13
+        self.temp_hp = 0
+        self.hit_dice = ["1d12"]
         self.proficiency_bonus = 2
         self.save_proficencies = []
         self.stats = [15, 14, 13, 12, 10, 8]
         self.skills = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.feats = ["magic initiate"]
+        self.heroic_insp = True
+        self.prepared_spells = []
+        self.equipment = []
 
 sheet = Main()
 sheet.mainloop()
