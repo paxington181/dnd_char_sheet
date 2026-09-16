039949b6185d7e778b9a275ed7d1eb34ea93ec6e
paxington181 <tsandrus@rogers.com>
2026-09-16 09:08:00 -0400

    Coding while tired is a bad idea.

--- test.py
+++ test.py
@@ -1,1 +1,2 @@
-barbarian = dict("primary_ability" = "strength", "hit_dice" = "1d12", "saves" = ["str", "con"], "weapon_prof" = ["simple", "martial"], "armor_prof" = ["light", "medium", "shield"])
+barbarian = dict(primary_ability = "strength", hit_dice = "1d12", saves = ["str", "con"], weapon_prof = ["simple", "martial"], armor_prof = ["light", "medium", "shield"])
+print(barbarian)
