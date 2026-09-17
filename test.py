66eca02ecc6ad660a17c0e00080dd7a2efc5ae07
paxington181 <tsandrus@rogers.com>
2026-09-17 13:06:43 -0400

    Barbarian data set built for level 1

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
-barbarian = dict(primary_ability = "strength", hit_dice = "1d12", saves = ["str", "con"], weapon_prof = ["simple", "martial"], armor_prof = ["light", "medium", "shield"])
+barbarian = dict(primary_ability = "strength", hit_dice = "1d12", saves = ["str", "con"], weapon_prof = ["simple", "martial"], armor_prof = ["light", "medium", "shield"],
+                 start_equip = dict(A = ["Greataxe", "Handaxe", "Handaxe", "Handaxe", "Handaxe", ("GP", 15)], B = [("GP", 15)]),
+                 lvl_1 = dict(features = ["rage", "unarmored defense", "weapon mastery"], rages = 2, rage_damage = 2, weap_mast = 2)
+                 )
 print(barbarian)
